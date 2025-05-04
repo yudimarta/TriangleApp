14a48fea4b8d49b5177a5e4df183ae13b05d179f
yudiara <yudi_m_arianto@telkomsel.co.id>
2025-05-04 17:18:34 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Yudi Marta on 04/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
